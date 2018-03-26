289d5184be9159edfe539eda78ad3111f30047ab
Nerldy_ <arnodhis@gmail.com>
2018-03-26 13:14:09 +0300

    add test_get_book function to test GET /books returns JSON

--- app-test.py
+++ app-test.py
@@ -10,6 +10,12 @@ class HelloBooksAPITestCase(unittest.TestCase):
 		"""Test POST /books will create a book"""
 		pass
 
+	def test_get_book(self):
+		"""test GET /books returns all books in JSON format. If no books available it returns {"books": "None"}"""
+		tester = app.test_client(self)
+		response = tester.get('/books', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
 	unittest.main()
