ba19948d39b3f27909d9b631bf69d03d6d91ccd7
Nerldy_ <arnodhis@gmail.com>
2018-03-26 15:47:05 +0300

    add book list to hold dict array and act like a database add BooksResource class handler for GET /books and return all books add BookResource class to handle GET, POST, PUT, DELETE /book

--- app.py
+++ app.py
@@ -4,15 +4,42 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+books = []
 
-class Books(Resource):
+
+class BooksResource(Resource):
 	"""This class represents /books"""
 
 	def get(self):
-		return {"book": "Hello Books"}
+		return books
 
 
-api.add_resource(Books, '/books')
+class BookResource(Resource):
+	"""This class represents /books/<book_id>"""
+
+	def get(self, book_id=None):
+		""""GET /book/<book_id>"""
+
+		# iterate the books list to search for a book through ID
+		for book in books:
+			if book[book_id] == book_id:
+				return book
+
+			# return none if book doesn't exists
+			return {"book": book_id}
+
+	def post(self, book_id=None):
+		"""POST /book/<book_id>"""
+
+		# create a book
+		book = {"book_id": book_id, "book_title": "Title of Book"}
+		# add it to the book list
+		books.append(book)
+		return book
+
+
+api.add_resource(BooksResource, '/books')
+api.add_resource(BookResource, '/book/<book_id>')
 
 if __name__ == '__main__':
 	app.run(debug=1)
