372b5e7d9c46e790f2eaf4bbf5e2bc01e5c19bf4
Nerldy_ <arnodhis@gmail.com>
2018-03-26 08:39:52 +0300

    remove flask_restful and use plain flask.

--- app-test.py
+++ app-test.py
@@ -1,14 +1,20 @@
-from app import Books
+from app import app
+
 import unittest
 
 
-class HelloBooksTestCases(unittest.TestCase):
+class BasicTestCase(unittest.TestCase):
 
-	def test_get_function(self):
-		"""Should return get function"""
-		result = Books.get(self)
-		self.assertEqual(result, Books.get(self))
+	def test_index(self):
+		"""
+		:return: Hello World string
+		"""
+		tester = app.test_client(self)
+		response = tester.get('/', content_type='html/text')
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.data, b'Hello World')
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
 	unittest.main()
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class Books(Resource):
-	def get(self):
-		pass
+@app.route('/')
+def index():
+	return "Hello World"
+
+
+if __name__ == '__main__':
+	app.run(debug=1)
