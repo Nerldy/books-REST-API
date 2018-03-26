988123ce360e0e430259f97ae3da0bc86310d745
Nerldy_ <arnodhis@gmail.com>
2018-03-26 08:44:49 +0300

    add get_all_books function for GET /books

--- app-test.py
+++ app-test.py
@@ -14,6 +14,13 @@ class BasicTestCase(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertEqual(response.data, b'Hello World')
 
+	def test_get_all_books(self):
+		"""
+		:return: get_all_books function
+		"""
+		tester = app.test_client(self)
+		response = tester.get('/books', content_type='html/text')
+		self.assertEqual(response.data, b"get all books")
 
 
 if __name__ == '__main__':
--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+	"""
+	:return: Home page
+	"""
 	return "Hello World"
+
+
+@app.route('/books')
+def get_all_books():
+	"""
+	:return: All books
+	"""
+	return 'get all books'
 
 
 if __name__ == '__main__':
