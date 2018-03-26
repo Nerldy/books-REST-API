76fe3d0a73acc45a1efa3990a406f114355a06d0
Nerldy_ <arnodhis@gmail.com>
2018-03-26 12:35:51 +0300

    add test_book_creation function to test POST /books

--- app-test.py
+++ app-test.py
@@ -3,24 +3,12 @@ from app import app
 import unittest
 
 
-class BasicTestCase(unittest.TestCase):
+class HelloBooksAPITestCase(unittest.TestCase):
+	"""Class represents Hello Books test cases"""
 
-	def test_index(self):
-		"""
-		:return: Hello World string
-		"""
-		tester = app.test_client(self)
-		response = tester.get('/', content_type='html/text')
-		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.data, b'Hello World')
-
-	def test_get_all_books(self):
-		"""
-		:return: get_all_books function
-		"""
-		tester = app.test_client(self)
-		response = tester.get('/books', content_type='html/text')
-		self.assertEqual(response.data, b"get all books")
+	def test_book_creation(self):
+		"""Test POST /books will create a book"""
+		pass
 
 
 if __name__ == '__main__':
