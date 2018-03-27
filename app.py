771d1871efd82c145822bad530f91767ba25789e
Nerldy_ <arnodhis@gmail.com>
2018-03-27 11:36:47 +0300

    add GET, PUT, DELETE, POST /book end points

--- app.py
+++ app.py
@@ -6,9 +6,24 @@ api = Api(app)
 
 
 class AllBooksResource(Resource):
+	"""Handle GET /books"""
 
 	def get(self, book_id):
 		return {"book": book_id}
 
 
+class SingleBookResource(Resource):
+	"""Handle GET, PUT, DELETE, POST /book"""
+
+	def get(self, book_id):
+		pass
+
+	def post(self, book_id):
+		pass
+
+
+# API Resources
 api.add_resource(AllBooksResource, '/books')
+api.add_resource(SingleBookResource, '/book/<book_id>')
+
+app.run(port=5000, debug=1)
