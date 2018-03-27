6e2e060f715463e3d32ab70bde1527f0cf917cc1
Nerldy_ <arnodhis@gmail.com>
2018-03-27 13:34:38 +0300

    add logic for get and post methods

--- app.py
+++ app.py
@@ -4,22 +4,44 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+# Books list
+books = []
+
 
 class AllBooksResource(Resource):
 	"""Handle GET /books"""
 
-	def get(self, book_id):
-		return {"book": book_id}
+	def get(self):
+		if books:
+			return {"books": books}
+
+		return {"books": "No books found"}
 
 
 class SingleBookResource(Resource):
 	"""Handle GET, PUT, DELETE, POST /book"""
 
 	def get(self, book_id):
-		pass
+
+		for book in books:
+			if book["book_id"] == book_id:
+				return {"book_id": book[book_id]}, 200
+			return {"message": f"Book with id {book_id} not found"}, 404
 
 	def post(self, book_id):
-		pass
+
+		for book in books:
+			if book["book_id"] == book_id:
+				return {"message": f"Book with id {book_id} already exists."}, 400
+
+		book = {"book_id": book_id}
+		books.append(book)
+		return book, 201
+
+	def delete(self, book_id):
+		global books
+		books = list(filter(lambda x: x["book_id"] != book_id, books))
+		return {"message": "book deleted"}
 
 
 # API Resources
