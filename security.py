243ef299f410e6ed39bb9b8712beb28d940b239c
Nerldy_ <arnodhis@gmail.com>
2018-03-27 15:44:24 +0300

    add user mapping to emulate fake database to authenticate the user

--- security.py
+++ security.py
@@ -1,2 +1,37 @@
+# dummy user
+users = [
+	{
+		"id": 1,
+		"username": 'nerldy',
+		'password': "mic check"
+	}
+]
+
+username_mapping = {
+	"nerldy": {
+		"id": 1,
+		"username": 'nerldy',
+		'password': "mic check"
+	}
+
+}
+
+userid_mapping = {
+	1: {
+		"id": 1,
+		"username": 'nerldy',
+		'password': "mic check"
+	}
+
+}
+
+
 def authentication(username, password):
-	pass
+	user = username_mapping.get(username, None)
+	if user and user.password == password:
+		return user
+
+
+def identity(payload):
+	user_id = payload['identity']
+	return userid_mapping.get(user_id, None)
