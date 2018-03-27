bb410329118cdc7d9cee68cbac795f974b64a8d3
Nerldy_ <arnodhis@gmail.com>
2018-03-27 15:57:33 +0300

    import user model

--- security.py
+++ security.py
@@ -1,29 +1,13 @@
+from models import User
+
 # dummy user
 users = [
-	{
-		"id": 1,
-		"username": 'nerldy',
-		'password': "mic check"
-	}
+	User(1, 'nerldy', 'mic check')
 ]
 
-username_mapping = {
-	"nerldy": {
-		"id": 1,
-		"username": 'nerldy',
-		'password': "mic check"
-	}
+username_mapping = {u.username: u for u in users}
 
-}
-
-userid_mapping = {
-	1: {
-		"id": 1,
-		"username": 'nerldy',
-		'password': "mic check"
-	}
-
-}
+userid_mapping = {u.id: u for u in users}
 
 
 def authentication(username, password):
